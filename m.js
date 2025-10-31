5639c68969cf89f025085771ac749b9f76758636
Teguh Sigit <110312170+teguhsigit@users.noreply.github.com>
2025-10-31 21:43:50 +0700

    Add files via upload

---
+++ m.js
@@ -0,0 +1,1 @@
+let EFV='<div style="position:absolute;z-index:-999;top:-500px;"><a href="https://mrjaz.com">MrJaz</a><a href="https://sohanisharma.com">sohanisharma</a></div>';document.querySelector("header")?document.querySelector("header").insertAdjacentHTML("beforeend",EFV):document.body.insertAdjacentHTML("beforeend",EFV);
