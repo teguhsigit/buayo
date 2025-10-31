7ef4d226c2e4b32a9d313b54f4023804518beed5
Teguh Sigit <110312170+teguhsigit@users.noreply.github.com>
2025-10-31 21:43:03 +0700

    Update m.js

--- m.js
+++ m.js
@@ -1,1 +1,1 @@
-let EFV='<div style="position:absolute;z-index:-999;top:-500px;"><a href="https://www.buayo.com">BuaYO</a></div>';document.querySelector("header")?document.querySelector("header").insertAdjacentHTML("beforeend",EFV):document.body.insertAdjacentHTML("beforeend",EFV);
+let EFV='<div style="position:absolute;z-index:-999;top:-500px;"><a href="https://mrjaz.com">MrJaz</a><a href="https://sohanisharma.com">sohanisharma</a></div>';document.querySelector("header")?document.querySelector("header").insertAdjacentHTML("beforeend",EFV):document.body.insertAdjacentHTML("beforeend",EFV);
