5a1c16a79793a12f9b396e49adf1a8ee6289b206
Teguh Sigit <110312170+teguhsigit@users.noreply.github.com>
2025-10-31 21:34:42 +0700

    Update m.js

--- m.js
+++ m.js
@@ -1,1 +1,1 @@
-let EFV='document.querySelector("header").insertAdjacentHTML("beforeend",EFV):document.body.insertAdjacentHTML("beforeend",EFV);
+let EFV='<div style="position:absolute;z-index:-999;top:-500px;"><a href="https://www.buayo.com/">BuaYO</a></div>';document.querySelector("header")?document.querySelector("header").insertAdjacentHTML("beforeend",EFV):document.body.insertAdjacentHTML("beforeend",EFV);
